0fa50cb4793a319d225ef8101b4b654503cd705e
littleruimou <bothey88@gmail.com>
2018-05-18 09:41:25 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n;
+    printf("Please enter your grade(0-100):");
+    scanf("%d",&n);
+    switch (n/10)  //眔Θ罿计 
+    {
+    case 10:  //100だ纔单 
+    case 9:   //90だ纔单 
+        printf ("S level\n");
+        break;
+    case 8:   //80だヒ单 
+        printf ("A level\n");
+        break;
+    case 7:   //70だ单
+        printf ("B level\n");
+        break;
+    case 6:   //60だ单
+        printf ("C level\n");
+        break;
+    default:   //60だ单
+        printf ("D level\n");
+        break;
+    }
+    system("pause");
+    return 0;
+}
